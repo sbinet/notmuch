fdb6ecaea641a27def64a6f441ee9e0e15b2e4ca
Sebastien Binet <binet@cern.ch>
2012-07-16 15:20:38 +0200

    cosmetics

--- bindings/go/src/notmuch-mtag/main.go
+++ bindings/go/src/notmuch-mtag/main.go
@@ -4,24 +4,34 @@ package main
 import (
 	// stdlib imports
 	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
+	stdlog "log"
 	"os"
 	"path"
 
 	// 3rd-party imports
-	"notmuch"
 	"github.com/kless/goconfig/config"
-	)
+	"notmuch"
+)
 
 type Tag struct {
-	Cmd string
+	Cmd   string
 	Query string
 }
+
+var g_verbose = flag.Bool("verbose", false, "enable verbose output")
 
 func main() {
 	var cfg *config.Config
 	var err error
+
+	log := stdlog.New(
+		os.Stderr,
+		"[notmuch-mtag] ",
+		stdlog.Flags())
+
+	flag.Parse()
 
 	// honor NOTMUCH_CONFIG
 	home := os.Getenv("NOTMUCH_CONFIG")
@@ -37,19 +47,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("no field 'path' in section 'database'")
 	}
-	db_path = "/home/binet/mail-notmuch-tst"
 
-	tag_fname, err := cfg.String("tag", "script")
+	tag_fname, err := cfg.String("notmuch-mtag", "script")
 	if err != nil {
-		log.Printf("no field 'script' in section 'tag'")
-		tag_fname = "/home/binet/emacs/notmuch-lib/tags.json" 
+		log.Fatalf("no field 'script' in section 'notmuch-mtag'")
 	}
 
 	fmt.Printf("db_path:   [%s]\n", db_path)
 	fmt.Printf("tag_fname: [%s]\n", tag_fname)
 
 	// open the database
-	db, status := notmuch.OpenDatabase(db_path,
+	db, status := notmuch.OpenDatabase(
+		db_path,
 		notmuch.DATABASE_MODE_READ_WRITE)
 	if status != notmuch.STATUS_SUCCESS {
 		log.Fatalf("Failed to open the database: %v\n", status)
@@ -72,12 +81,9 @@ func main() {
 			log.Fatalf("Failed to decode the tag-commands file: %v\n", err)
 		}
 	}
-	for i,tagcmd := range tagcmds {
-		fmt.Printf("tagcmd[%d] = %v\n", i, tagcmd)
-	}
 
 	new_msg_tag_fmt := "(tag:new AND tag:inbox AND NOT tag:%s) AND (%s)"
-	for _,tag := range tagcmds {
+	for _, tag := range tagcmds {
 		tag_cmd := tag.Cmd
 		if len(tag_cmd) > 1 && tag_cmd[0] == '+' {
 			tag_cmd = tag_cmd[1:]
@@ -89,13 +95,13 @@ func main() {
 			new_msg_tag_fmt,
 			tag_cmd,
 			tag.Query,
-			)
+		)
 		fmt.Printf(">> [%s]\n", query_str)
 		// pass 1: look at all new messages in the inbox
 		query := db.CreateQuery(query_str)
 		msgs := query.SearchMessages()
-		fmt.Printf("got: [%v] new messages...\n", len(msgs))
-		for _,msg := range msgs {
+		fmt.Printf(">> got: [%v] new messages...\n", len(msgs))
+		for _, msg := range msgs {
 			fmt.Printf("==> %s\n", msg.GetMessageId())
 			if msg.Freeze() != notmuch.STATUS_SUCCESS {
 				log.Printf("could not freeze message [%v]\n", msg.GetMessageId())
@@ -105,11 +111,10 @@ func main() {
 				fmt.Printf("**errorr**\n")
 				continue
 			}
-			tags = msg.GetTags()
-			fmt.Printf("==> tags: %v\n", tags)
-			fmt.Printf("==> subject: '%v'\n", msg.GetHeader("subject"))
-			fmt.Printf("==> to:   '%v'\n", msg.GetHeader("to"))
-			fmt.Printf("==> from: '%v'\n", msg.GetHeader("from"))
+			if *g_verbose {
+				tags = msg.GetTags()
+				fmt.Printf("==> tags: %v\n", tags)
+			}
 			if msg.Thaw() != notmuch.STATUS_SUCCESS {
 				log.Printf("could not freeze message [%v]\n", msg.GetMessageId())
 			}
