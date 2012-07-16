4cae0ea872f2e174c6a99575e4dce5f2808ce57c
Sebastien Binet <binet@cern.ch>
2012-07-16 18:18:48 +0200

    use the log package + allow multiple command in the json-Cmd

--- bindings/go/src/notmuch-mtag/main.go
+++ bindings/go/src/notmuch-mtag/main.go
@@ -9,6 +9,7 @@ import (
 	stdlog "log"
 	"os"
 	"path"
+	"strings"
 
 	// 3rd-party imports
 	"github.com/kless/goconfig/config"
@@ -20,6 +21,27 @@ type Tag struct {
 	Query string
 }
 
+func build_tag_cmd(cmd string) (cmds []string, err error) {
+	cmds = strings.Split(cmd, " ")
+	if len(cmds) <= 0 {
+		err = fmt.Errorf("invalid tag-cmd [%s]", cmd)
+		return nil, err
+	}
+	for idx,c := range cmds {
+		if len(c) <= 1 {
+			err = fmt.Errorf("invalid tag-cmd at index #%d: [%s]", idx, c)
+			return nil, err
+		}
+		switch c[0] {
+		case '+', '-':
+			// ok.
+		default:
+			err = fmt.Errorf("invalid tag-cmd at index #%d: [%s]", idx, c)
+			return nil, err
+		}
+	}
+	return cmds, err
+}
 var g_verbose = flag.Bool("verbose", false, "enable verbose output")
 
 func main() {
@@ -32,6 +54,8 @@ func main() {
 		stdlog.Flags())
 
 	flag.Parse()
+	log.Printf(":: notmuch-mtag...\n")
+	log.Printf("verbose:  %v\n", *g_verbose)
 
 	// honor NOTMUCH_CONFIG
 	home := os.Getenv("NOTMUCH_CONFIG")
@@ -53,8 +77,8 @@ func main() {
 		log.Fatalf("no field 'script' in section 'notmuch-mtag'")
 	}
 
-	fmt.Printf("db_path:   [%s]\n", db_path)
-	fmt.Printf("tag_fname: [%s]\n", tag_fname)
+	log.Printf("db_path:   [%s]\n", db_path)
+	log.Printf("tag_fname: [%s]\n", tag_fname)
 
 	// open the database
 	db, status := notmuch.OpenDatabase(
@@ -63,6 +87,7 @@ func main() {
 	if status != notmuch.STATUS_SUCCESS {
 		log.Fatalf("Failed to open the database: %v\n", status)
 	}
+	defer db.Close()
 
 	// open the tag commands
 	tagfile, err := os.Open(tag_fname)
@@ -82,43 +107,94 @@ func main() {
 		}
 	}
 
-	new_msg_tag_fmt := "(tag:new AND tag:inbox AND NOT tag:%s) AND (%s)"
+	{
+		// remove phony messages (before 1980)
+		query := db.CreateQuery("tag:new AND tag:inbox AND ..-0")
+		msgs := query.SearchMessages()
+		for _, msg := range msgs {
+			for _,tag := range []string{"new", "inbox", "unread"} {
+				if msg.Freeze() != notmuch.STATUS_SUCCESS {
+					log.Printf("could not freeze message [%v]\n", msg.GetMessageId())
+				}
+				if msg.RemoveTag(tag) != notmuch.STATUS_SUCCESS {
+					log.Printf("could not apply tag -%s to message id=%v\n", 
+						tag,
+						msg.GetMessageId())
+				}
+				if msg.Thaw() != notmuch.STATUS_SUCCESS {
+					log.Printf("could not freeze message [%v]\n", msg.GetMessageId())
+				}
+			}
+		}
+	}
+
+	new_msg_tag_fmt := "(tag:new AND tag:inbox AND NOT (%s)) AND (%s)"
 	for _, tag := range tagcmds {
-		tag_cmd := tag.Cmd
-		if len(tag_cmd) > 1 && tag_cmd[0] == '+' {
-			tag_cmd = tag_cmd[1:]
-		} else {
-			log.Printf("invalid tag command: [%s]\n", tag_cmd)
+		tag_cmds, err := build_tag_cmd(tag.Cmd)
+		if err != nil {
+			log.Printf("error: %v\n", err)
 			continue
+		}
+		tag_cmd_str := make([]string, len(tag_cmds))
+		for i,tag_cmd_i := range tag_cmds {
+			tag_cmd_str[i] = "tag:"+tag_cmd_i[1:]
 		}
 		query_str := fmt.Sprintf(
 			new_msg_tag_fmt,
-			tag_cmd,
+			strings.Join(tag_cmd_str, " AND "),
 			tag.Query,
 		)
-		fmt.Printf(">> [%s]\n", query_str)
-		// pass 1: look at all new messages in the inbox
+
+		// look at all new messages in the inbox
 		query := db.CreateQuery(query_str)
 		msgs := query.SearchMessages()
-		fmt.Printf(">> got: [%v] new messages...\n", len(msgs))
+		if len(msgs) > 0 {
+			log.Printf(">> [%s]\n", query_str)
+			log.Printf(">> got: [%v] new messages...\n", len(msgs))
+		}
 		for _, msg := range msgs {
-			fmt.Printf("==> %s\n", msg.GetMessageId())
+			if *g_verbose {
+				log.Printf("==> %s\n", msg.GetMessageId())
+			}
 			if msg.Freeze() != notmuch.STATUS_SUCCESS {
 				log.Printf("could not freeze message [%v]\n", msg.GetMessageId())
 			}
 			tags := msg.GetTags()
-			if msg.AddTag(tag_cmd) != notmuch.STATUS_SUCCESS {
-				fmt.Printf("**errorr**\n")
-				continue
+			for _,tag_cmd := range tag_cmds {
+				switch tag_cmd[0] {
+					case '+':
+					if msg.AddTag(tag_cmd[1:]) != notmuch.STATUS_SUCCESS {
+						log.Printf("**errorr**\n")
+						continue
+					}
+					case '-':
+					if msg.RemoveTag(tag_cmd[1:]) != notmuch.STATUS_SUCCESS {
+						log.Printf("**errorr**\n")
+						continue
+					}
+				}
 			}
 			if *g_verbose {
 				tags = msg.GetTags()
-				fmt.Printf("==> tags: %v\n", tags)
+				log.Printf("==> tags: %v\n", tags)
 			}
 			if msg.Thaw() != notmuch.STATUS_SUCCESS {
 				log.Printf("could not freeze message [%v]\n", msg.GetMessageId())
 			}
 		}
 	} //> tag cmds
+
+	// remove the 'new' tag
+	query := db.CreateQuery("tag:new AND tag:inbox")
+	msgs := query.SearchMessages()
+	log.Printf(">> applying tag -new...\n")
+	log.Printf(">> got: [%v] new messages...\n", len(msgs))
+	for _, msg := range msgs {
+		if msg.RemoveTag("new") != notmuch.STATUS_SUCCESS {
+			log.Printf("could not 'un-new' message id=%v\n", msg.GetMessageId())
+		}
+	}
+	log.Printf(">> applying tag -new... [done]\n")
+	log.Printf(":: notmuch-mtag...[done]\n")
 	return
 }
